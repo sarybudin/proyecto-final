8d5eb69a8a6ea02427b1db2a2903adcb31787c96
sarybudin <sarybudin@gmail.com>
2021-12-28 23:32:29 +0000

    modelos

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,77 @@
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Psicologo(db.Model):
+    __tablename__ = 'psicologo'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+    nombre = db.Column(db.String(120), unique=True, nullable=False)
+    telefono = db.Column(db.String(12), unique=True, nullable=False)
+    direccion_comercial = db.Column(db.String(120), unique=True, nullable=False)
+    paciente = db.relationship("Paciente", back_populates="psicologo")
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "nombre": self.nombre,
+            "telefono": self.telefono,
+            "direccion_comercial": self.direccion_comercial,
         }
+
+class Paciente(db.Model):
+    __tablename__ = 'paciente'
+    id = db.Column(db.Integer, primary_key=True)
+    telefono = db.Column(db.String(12), unique=True, nullable=False)
+    psicologo_id = db.Column(db.Integer,db.ForeignKey('psicologo.id'))
+    fecha_nacimiento = db.Column(db.Date, nullable=True)
+    nombre = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    direccion = db.Column(db.String(120), unique=True, nullable=True)
+    diagnóstico = db.Column(db.String(120), unique=True, nullable=True)
+    estado_civil = db.Column(db.String(120), unique=True, nullable=True)
+    nro_hijos = db.Column(db.Integer, unique=True, nullable=True)
+    nacionalidad = db.Column(db.String(120), unique=True, nullable=True)
+    psicologo = db.relationship("Psicologo", back_populates="paciente")
+    bot = db.relationship("Bot", back_populates="paciente")
+    username = db.Column(db.String(120), unique=True, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "telefono": self.telefono,
+            "psicologo_id": self.psicologo_id,
+            "fecha_nacimiento": self.fecha_nacimiento,
+            "nombre": self.nombre,
+            "email": self.email,
+            "direccion": self.direccion,
+            "diagnostico": self.diagnóstico,
+            "estado_civil": self.estado_civil,
+            "nro_hijos": self.nro_hijos,
+            "nacionalidad": self.nacionalidad,
+            "username": self.username,
+            
+        }
+
+
+class Bot(db.Model):
+    __tablename__ = 'bot'
+    id = db.Column(db.Integer, primary_key=True)
+    respuesta = db.Column(db.String(120), unique=False, nullable=False)
+    paciente_id = db.Column(db.Integer,db.ForeignKey('paciente.id'))
+    fecha = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    paciente = db.relationship("Paciente", back_populates="bot")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "respuesta": self.respuesta,
+            "paciente_id": self.paciente_id,
+             "fecha": self.fecha,
+        }
+    
+
+
