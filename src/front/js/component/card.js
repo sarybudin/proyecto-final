4a83fc3a0663cd84ff9367a9a55a44e304c52567
germanguaimare <germanguaimare@gmail.com>
2022-01-01 14:04:07 +0000

    layout completo

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -5,37 +5,28 @@ import PropTypes from "prop-types";
 import "../../styles/card.css";
 import { Card, CardImg, CardBody, CardText, CardTitle } from "reactstrap";
 
-export const HomeCard = () => {
+export const HomeCard = (props) => {
   //  const { store, actions } = useContext(Context);
 
   return (
-    <div className="container-fluid">
-      <h1>ÁniBot!!</h1>
-      <Card className="Card">
-        <CardImg
-          className="CardImg"
-          alt="Card image cap"
-          src="https://dummyimage.com/400x300/000/fff.png&text=Imagen+1"
-          top
-          width="100%"
-        />
-        <CardBody>
-          <CardTitle tag="h5">Título</CardTitle>
-          <CardText>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
-          </CardText>
-        </CardBody>
-      </Card>
-    </div>
+    <Card className="Card">
+      <CardImg
+        className="CardImg"
+        alt="Card image cap"
+        src="https://dummyimage.com/400x300/000/fff.png&text=Imagen+1"
+        top
+        width="100%"
+      />
+      <CardBody>
+        <CardTitle tag="h5">{props.title}</CardTitle>
+        <CardText>{props.text}</CardText>
+      </CardBody>
+    </Card>
   );
 };
 
-/*HomeCard.propTypes = {
+HomeCard.propTypes = {
   title: PropTypes.string,
+  text: PropTypes.string,
+  image: PropTypes.string,
 };
-
-HomeCard.defaultProps = {
-  title: "Título",
-};*/
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,8 +8,26 @@ export const Home = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className="container-fluid">
-      <HomeCard />
+    <div className="container-fluid mt-5">
+      <div className="row text-center">
+        <h2>ÁniBot</h2>
+      </div>
+      <div className="row mt-5 d-flex justify-content-around" id="myrow">
+        <HomeCard
+          title="¿Cómo se siente hoy?"
+          text="Recibe a diario el estado de ánimo de tus pacientes"
+        />
+
+        <HomeCard
+          title="Complementa tu Análisis"
+          text="Suma métricas y gráficas de simple interpretación a tus diagnósticos"
+        />
+
+        <HomeCard
+          title="Lo que se viene"
+          text="Establece relaciones entre palabras clave y estados de ánimo"
+        />
+      </div>
     </div>
   );
 };
