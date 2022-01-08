fe4a49c04d4b16179fdcb128ef6a2919905407b3
Jflowers74 <91094557+Jflowers74@users.noreply.github.com>
2022-01-08 10:03:52 -0300

    formulario_base1

---
+++ src/front/js/component/Formulario.js
@@ -0,0 +1,84 @@
+import React, {Fragment, useState} from "react";
+
+const Formulario = () => {
+
+    const [paciente, setPaciente] = useState({
+        nombre: '',
+        apellido: '',
+        direccion: '',
+        email: '',
+        telefono: '',
+        telegram: '',
+        diagnostico: ''
+    })
+
+    const handleInputChange = (e) => {
+        setPaciente({
+            ...paciente,
+            [e.target.name] : e.target.value
+        })
+      console.log(paciente)    
+    }
+
+    const crearPaciente = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <Fragment>
+            <h1>Ingreso Pacientes</h1>
+            <form onSubmit={crearPaciente}> 
+                <div className="">
+                <input
+                    placeholder="Nombres"
+                    className="form-control"
+                    type="text"
+                    name="nombre"
+                    onChange={handleInputChange} />
+                <input
+                    placeholder="Apellidos"
+                    className="form-control"
+                    type="text"
+                    name="apellido"
+                    onChange={handleInputChange} />
+                <input
+                    placeholder="Ingrese direccion"
+                    className="form-control"
+                    name="direccion"
+                    onChange={handleInputChange} />
+                <input
+                    placeholder="e-mail"
+                    className="form-control"
+                    type="email"
+                    name="email"
+                    onChange={handleInputChange} />
+                <input
+                    placeholder="telefono"
+                    className="form-control"
+                    type="number"
+                    name="telefono"
+                    onChange={handleInputChange} />
+                <input
+                    placeholder="Telegram"
+                    className="form-control"
+                    type="number"
+                    name="telegram"
+                    onChange={handleInputChange} />
+                <input
+                    placeholder="Diagnostico"
+                    className="form-control"
+                    type="text"
+                    name="diagnostico"
+                    onChange={handleInputChange} />
+                </div>
+                <div>
+                    <button className="btn btn-primary" type="submit">Button</button>
+                </div>
+                
+            </form>
+        </Fragment>
+    );
+
+}
+
+export default Formulario;
---
+++ src/front/js/pages/registros.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import Formulario from "../component/Formulario";
+
+const Registros = ( ) => {
+    return (
+        <div className="container mt-5">
+            <Formulario />
+        </div>
+    );
+}
+
+export default Registros;
